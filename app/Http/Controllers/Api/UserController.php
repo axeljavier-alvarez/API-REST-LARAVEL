82cf892d64dc5331d11495e95fec8892ea602400
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-05-28 11:07:15 -0600

    funciones para api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+         return response()->json([
+            'message' => 'Listado de usuarios'
+         ]);
+    }
+
+
+    public function store()
+    {
+        return response()->json([
+            'message' => 'Usuario creado'
+
+        ]);
+    }
+
+    public function show($id)
+    {
+        return response()->json([
+            'message' => 'Usuario recuperado: '. $id
+        ]);
+    }
+
+    public function update($id)
+    {
+        return response()->json([
+            'message' => 'Usuario actualizado: '. $id
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        return response()->json([
+            'message' => 'Usuario eliminado: '. $id
+        ]);
+    }
+
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php   
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/', function () {
     return response()->json([
@@ -10,40 +11,49 @@ Route::get('/', function () {
 
 // listar registros
 
-Route::get('users', function(){
-    return response()->json([
-        'message' => 'Listado de usuarios'
-    ]);
-});
+// Route::get('users', function(){
+//     return response()->json([
+//         'message' => 'Listado de usuarios'
+//     ]);
+// });
+
+Route::get('users', [UserController::class, 'index']);
 
 // crear registros
-Route::post('users', function(){
-    return response()->json([
-        'message' => 'Usuario creado'
-    ]);
-});
+// Route::post('users', function(){
+//     return response()->json([
+//         'message' => 'Usuario creado'
+//     ]);
+// });
+
+Route::post('users', [UserController::class, 'store']);
 // recuperar registros
-Route::get('users/{id}', function($id){
-    return response()->json([
-        'message' => 'Usuario recuperado: '. $id
-    ]);
-});
+// Route::get('users/{id}', function($id){
+//     return response()->json([
+//         'message' => 'Usuario recuperado: '. $id
+//     ]);
+// });
+Route::get('users/{id}', [UserController::class, 'show']);
+
+
 // actualizar registros
-Route::put('users/{id}', function($id){
-    return response()->json([
-        'message' => 'Usuario actualizado: '. $id
-    ]);
-});
+// Route::put('users/{id}', function($id){
+//     return response()->json([
+//         'message' => 'Usuario actualizado: '. $id
+//     ]);
+// });
+Route::patch('users/{id}', [UserController::class, 'update']);
 
 // Route::patch('users/{id}', function($id){
 //     return response()->json([
 //         'message' => 'Usuario actualizado: '. $id
 //     ]);
 // });
-
+// Route::delete('uses')
 // eliminar registros
-Route::delete('users/{id}', function($id){
-    return response()->json([
-        'message' => 'Usuario eliminado: '. $id
-    ]); 
-});
+// Route::delete('users/{id}', function($id){
+//     return response()->json([
+//         'message' => 'Usuario eliminado: '. $id
+//     ]); 
+// });
+Route::delete('users/{id}', [UserController::class, 'destroy']);
