717df76f7665afa049c86708db2144272309b240
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-06-17 12:38:53 -0600

    cambios

--- routes/api.php
+++ routes/api.php
@@ -20,22 +20,16 @@ Route::post('/solicitudes/validar-paso', [SolicitudController::class, 'validarPa
 Route::apiResource('solicitudes', SolicitudController::class);
 Route::apiResource('tramites', TramiteController::class);
 Route::post('solicitudes/consultar', [SolicitudController::class, 'consultar']);
-
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('auth/logout', [AuthController::class, 'logout']);
 Route::post('auth/refresh', [AuthController::class, 'refresh']);
 Route::post('auth/me', [AuthController::class, 'me']);
-
 Route::apiResource('users', UserController::class);
 Route::apiResource('tasks', TaskController::class);
 Route::apiResource('categories', CategoryController::class);
-
-
 Route::apiResource('permissions', PermissionController::class);
-
 Route::apiResource('roles', RoleController::class);
-
 Route::post('posts/{post}/tags', [PostController::class, 'syncTags']);
 Route::apiResource('posts', PostController::class);
 Route::get('prueba', function(){
