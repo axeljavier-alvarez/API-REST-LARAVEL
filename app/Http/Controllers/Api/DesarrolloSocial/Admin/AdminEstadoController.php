ca4bee74bb15df955d9374c2573b437e0d31fbf1
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-07-06 14:57:37 -0600

    cambios

--- app/Http/Controllers/Api/DesarrolloSocial/Admin/AdminEstadoController.php
+++ app/Http/Controllers/Api/DesarrolloSocial/Admin/AdminEstadoController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api\DesarrolloSocial\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\DesarrolloSocial\Estado;
+use App\Models\DesarrolloSocial\Solicitud;
 use App\Http\Resources\DesarrolloSocial\Admin\AdminEstadoResource;
+use App\Http\Resources\DesarrolloSocial\SolicitudResource;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 
@@ -19,14 +21,31 @@ class AdminEstadoController extends Controller implements HasMiddleware
     {
         // $estados = Estado::withCount('solicitudes')->get();
         $estados = Estado::query()
-        ->withCount('solicitudes')
-        ->whereNotIn('nombre', [
-            
-            'Visita asignada',
-            'Visita realizada'
-        ])
-        ->get();
+            ->withCount('solicitudes')
+            ->whereNotIn('nombre', [
+
+                'Visita asignada',
+                'Visita realizada'
+            ])
+            ->get();
         return AdminEstadoResource::collection($estados);
     }
-   
+
+    public function cambiarEstado(Request $request, Solicitud $solicitud)
+    {
+        $request->validate([
+            'estado_id' => 'required|exists:estados,id'
+        ]);
+
+        $solicitud->update([
+            'estado_id' => $request->estado_id
+        ]);
+
+        return response()->json([
+            'message' => 'Estado actualizado correctamente.',
+            'solicitud' => new SolicitudResource(
+                $solicitud->load('estado')
+            )
+        ]);
+    }
 }
